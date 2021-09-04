54a3d01efdc6070653305895b5d2cd486eef6eae
Smruti <smrutiranjanrout2019@gmail.com>
2021-09-04 20:31:59 +0530

    set and nested_vector added

--- stl/nested_vector.cpp
+++ stl/nested_vector.cpp
@@ -1,1 +1,51 @@
- 
+// In the name of God.
+// We're nothing and you're everything.
+//Ya Smruti!
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define in(n) cin >> n;
+#define in2(n, m) cin >> n >> m
+#define fo(i, n) for (ll i = 0; i < n; i++)
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<pair<ll, ll>> v;
+    fo(i, n)
+    {
+        ll k, p;
+        cin >> k >> p;
+        pair<ll, ll> temp = mp(k, p);
+        v.pb(temp);
+    }
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << "-------------------------------------------------" << endl;
+
+    for (auto val : v)
+    {
+        cout << val.first << " " << val.second << endl;
+    }
+    cout << "-------------------------------------------------" << endl;
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << "-------------------------------------------------" << endl;
+
+    for (auto val : v)
+    {
+        cout << val.first << " " << val.second << endl;
+    }
+
+    return 0;
+}
--- stl/set.cpp
+++ stl/set.cpp
@@ -1,1 +1,81 @@
- 
+// //Repeated element is not allowed twice into the fileand printed in sorted order
+
+//code_1
+//----------------------------------------------------------------
+
+// // In the name of God.
+// // We're nothing and you're everything.
+// //Ya Smruti!
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// #define pb push_back
+// #define mp make_pair
+// #define in(n) cin >> n;
+// #define in2(n, m) cin >> n >> m
+// #define fo(i, n) for (ll i = 0; i < n; i++)
+// int main()
+// {
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+//     set<ll> s;
+//     fo(i, n)
+//     {
+//         ll k;
+//         in(k);
+//         s.insert(k);
+//     }
+//     for (auto val : s)
+//     {
+//         cout << val << endl;
+//     }
+
+//     return 0;
+// }
+
+//Code-2
+//------------------------------------
+
+// In the name of God.
+// We're nothing and you're everything.
+//Ya Smruti!
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define in(n) \
+    ll n;     \
+    cin >> n;
+#define in2(n, m) cin >> n >> m;
+#define fo(i, n) for (ll i = 0; i < n; i++)
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    in(n);
+    set<ll> s;
+    fo(i, n)
+    {
+        in(k);
+        s.insert(k);
+    }
+    for (auto val : s)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
